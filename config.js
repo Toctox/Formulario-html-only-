cd413c7d0910ba12dd2fb2e241921f2827ea6a9e
Toctox <102564725+Toctox@users.noreply.github.com>
2026-09-18 14:55:45 -0300

    Add JobPilot configuration

---
+++ config.js
@@ -0,0 +1,30 @@
+window.JOBPILOT_CONFIG = {
+  appName: 'JobPilot Natanael',
+  googleClientId: 'COLE_SEU_CLIENT_ID.apps.googleusercontent.com',
+  candidate: {
+    name: 'Natanael Martins Matos',
+    email: 'natanaelmartins351@gmail.com',
+    phone: '(27) 9 9987-9949',
+    degree: 'Pedagogia em andamento'
+  },
+  allowedCities: ['Serra', 'Vitória', 'Vila Velha'],
+  resumes: {
+    LOGISTICA: {
+      name: 'Curriculo_Logistica.pdf',
+      driveId: '126nOuDEW0jSgmenk35fvkOU7GOefW4-2'
+    },
+    ADMINISTRATIVO: {
+      name: 'Curriculo_Administrativo.pdf',
+      driveId: '19GJSkKztC2P9sNEj_IOLqWjsBu2Jkzvp'
+    },
+    RH_DP: {
+      name: 'Curriculo_RH_DP.pdf',
+      driveId: '1dIN6KU_1J0IetQXBuzGWnNagKkQMfwrv'
+    }
+  },
+  googleScopes: [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/gmail.send',
+    'https://www.googleapis.com/auth/drive.readonly'
+  ].join(' ')
+};
